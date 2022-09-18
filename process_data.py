a63eeaeda9fe5150571bcd6d3b27ab75105ab41b
junfuture1103 <junhak1103@naver.com>
2022-09-18 15:17:12 +0900

    Add data loc & remove_outlier.py

--- process_data.py
+++ process_data.py
@@ -1,8 +1,22 @@
 import pandas as pd
+import remove_outlier
 
-file_path = "./datasets"
+file_path = "./datasets/"
 file_name = "isang_test.csv"
 
 file_path += file_name
 
-df = pd.read_csv(file_path)
+data = pd.read_csv(file_path)
+
+#data.shape => rows x cols
+print(data.shape)
+print(data)
+
+#column 나누기 loc[행, 열]
+samples = data.loc[:, '이름' : '성별']
+sample = data.loc[:, '구매액']
+
+#이상치 제거
+sample = remove_outlier.remove_out(sample)
+
+print(sample)
